92fd2c4ce90a2ea1b088b72d24f019860c4b85c0
Na Wei <na.wei95@hotmail.com>
2021-01-28 13:04:44 +0100

    chatroom working

--- team-quiz-mvp/src/components/ChatRoom.js
+++ team-quiz-mvp/src/components/ChatRoom.js
@@ -4,7 +4,7 @@ import ChatMessage from "./ChatMessage";
 import firebase, { auth, firestore } from "../utils/firebase";
 import addMessage from "../utils/addMessage";
 import startSession from "../utils/startSession";
-
+import { Card, Form, Button, Row, Col } from "react-bootstrap";
 const ChatRoom = ({ user }) => {
   const dummy = useRef();
   const messagesRef = firestore.collection("messages");
@@ -28,31 +28,35 @@ const ChatRoom = ({ user }) => {
   };
 
   return (
-    <div className="chat-bg w-full sm:w-2/3 p-2 rounded">
-      <div className="overflow-y-auto h-screen-90">
-        {messages &&
-          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
-        <span ref={dummy}></span>
-      </div>
-
-      <form onSubmit={sendMessage} className="pt-3 w-full inline-flex">
-        <input
-          className="rounded-3xl px-3 w-full py-1 outline-none focus:shadow"
-          value={formValue}
-          onChange={(e) => setFormValue(e.target.value)}
-          placeholder="Say something"
-        />
-        <button
-          className={`material-icons p-2 mx-2 bg-white rounded-full transition-all duration-75 ease-in-out text-xl ${
-            !formValue || "text-pink-700 hover:text-pink-900"
-          }`}
-          type="submit"
-          disabled={!formValue}
-        >
-          send
-        </button>
-      </form>
-    </div>
+    <Card border="dark" style={{ width: "18rem" }}>
+      <Card.Header>Chat Room</Card.Header>
+      <Card.Body>
+        <Card.Text>
+          {messages &&
+            messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+          <span ref={dummy}></span>
+        </Card.Text>
+        <Card.Text>
+          <Form onSubmit={sendMessage} className="pt-3 w-full inline-flex">
+            <Row>
+              <Col xs={12} md={8}>
+                <Form.Control
+                  className="rounded-3xl px-3 w-full py-1 outline-none focus:shadow"
+                  value={formValue}
+                  onChange={(e) => setFormValue(e.target.value)}
+                  placeholder="Say something"
+                />
+              </Col>
+              <Col xs={6} md={4}>
+                <Button type="submit" disabled={!formValue}>
+                  send
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+        </Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
 
