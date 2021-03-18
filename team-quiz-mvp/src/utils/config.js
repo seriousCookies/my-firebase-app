ed35a62163427795596aad261d9497dba66edab5
Na Wei <na.wei95@hotmail.com>
2021-03-18 11:00:10 +0100

    config.js

---
+++ team-quiz-mvp/src/utils/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA4dlI41i8A1qRJWZjEbHDe3U9vLGoZcvU",
+  authDomain: "my-first-firebase-8edd4.firebaseapp.com",
+  databaseURL: "https://my-first-firebase-8edd4.firebaseio.com",
+  projectId: "my-first-firebase-8edd4",
+  storageBucket: "my-first-firebase-8edd4.appspot.com",
+  messagingSenderId: "1095175177705",
+  appId: "1:1095175177705:web:66adf1e9e0e4a150f7f778",
+  measurementId: "G-5VQBK81VRP",
+};
